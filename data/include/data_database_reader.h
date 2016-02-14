ffda0529a0f873888565fd4bea173cf633044fce
A. Warnke <andreas.warnke@gmx.net>
2016-02-14 18:39:14 +0100

    accessor classes to database defined

---
+++ data/include/data_database_reader.h
@@ -0,0 +1,51 @@
+/* File: data_database_reader.h; Copyright and License: see below */
+
+#ifndef DATA_DATABASE_READER_H
+#define DATA_DATABASE_READER_H
+
+/* public file for the doxygen documentation: */
+/*! \file */
+
+#include "data_database.h"
+#include <stdio.h>
+#include <sqlite3.h>
+#include <stdbool.h>
+
+/*!
+ *  \brief all data attributes needed for the database functions
+ */
+struct data_database_reader_struct {
+    data_database_t *database;
+};
+
+typedef struct data_database_reader_struct data_database_reader_t;
+
+/*!
+ *  \brief initializes the data_database_reader_t struct
+ */
+void data_database_reader_init ( data_database_reader_t *this_, data_database_t *database );
+
+/*!
+ *  \brief destroys the data_database_reader_t struct
+ */
+void data_database_reader_destroy ( data_database_reader_t *this_ );
+
+
+#endif  /* DATA_DATABASE_READER_H */
+
+
+/*
+Copyright 2016-2016 Andreas Warnke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
---
+++ data/source/data_database_writer.c
@@ -0,0 +1,39 @@
+/* File: data_database_writer.c; Copyright and License: see below */
+
+#include "data_database_writer.h"
+#include "trace.h"
+#include "log.h"
+
+
+void data_database_writer_init ( data_database_writer_t *this_, data_database_t *database )
+{
+    TRACE_BEGIN();
+    
+    (*this_).database = database;
+    
+    TRACE_END();
+}
+
+void data_database_writer_destroy ( data_database_writer_t *this_ )
+{
+    TRACE_BEGIN();
+    
+    TRACE_END();
+}
+
+
+/*
+Copyright 2016-2016 Andreas Warnke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
