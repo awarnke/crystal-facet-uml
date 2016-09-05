e9210f9e79a77afcf9f05e9a702eadebfacd5e33
A. Warnke <andreas.warnke@gmx.net>
2016-09-05 17:40:37 +0200

    all tests reactivated

--- main/unittest/unittest_main.c
+++ main/unittest/unittest_main.c
@@ -25,12 +25,10 @@ int main (int argc, char *argv[]) {
     TestRunner_runTest( data_change_notifier_test_get_list() );
     TestRunner_runTest( data_database_listener_test_get_list() );
     TestRunner_runTest( data_json_tokenizer_test_get_list() );
-    /*
     TestRunner_runTest( ctrl_diagram_controller_test_get_list() );
     TestRunner_runTest( ctrl_classifier_controller_test_get_list() );
     TestRunner_runTest( ctrl_consistency_checker_test_get_list() );
     TestRunner_runTest( ctrl_undo_redo_list_test_get_list() );
-    */
     TestRunner_end();
 
     TRACE_INFO( "--------------------" );
