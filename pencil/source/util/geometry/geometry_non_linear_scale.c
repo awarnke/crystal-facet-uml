b1cc60d678576de6b41b13ccfb218e5baa91a226
A. Warnke <andreas.warnke@gmx.net>
2016-10-27 21:07:31 +0200

    geometry_non_linear_scale_add_order implemented

--- pencil/source/util/geometry/geometry_non_linear_scale.c
+++ pencil/source/util/geometry/geometry_non_linear_scale.c
@@ -3,8 +3,10 @@
 #define __STDC_LIMIT_MACROS
 #include "util/geometry/geometry_non_linear_scale.h"
 #include "trace.h"
+#include "log.h"
 #include <assert.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 void geometry_non_linear_scale_init ( geometry_non_linear_scale_t *this_, double lower_bound, double upper_bound )
 {
@@ -29,7 +31,7 @@ void geometry_non_linear_scale_trace ( geometry_non_linear_scale_t *this_ )
     TRACE_INFO_INT( "- num_points:", (*this_).num_points );
     for ( uint32_t pos = 0; pos < (*this_).num_points; pos ++ )
     {
-        TRACE_INFO_INT_INT( "- location,order:", (int)((*this_).location[pos]), (*this_).order[pos] );
+        TRACE_INFO_INT_INT( "- location-%, order:", (int)((*this_).location[pos]*100.0), (*this_).order[pos] );
     }
 
     TRACE_END();
@@ -38,6 +40,55 @@ void geometry_non_linear_scale_trace ( geometry_non_linear_scale_t *this_ )
 void geometry_non_linear_scale_add_order ( geometry_non_linear_scale_t *this_, int32_t order )
 {
     TRACE_BEGIN();
+    assert( (*this_).num_points <= GEOMETRY_NON_LINEAR_SCALE_MAX_POINTS );
+
+    /* check for duplicates and for possible insert position */
+    bool duplicate = false;
+    uint32_t insert_pos = 0;
+    for ( uint32_t pos = 0; pos < (*this_).num_points; pos ++ )
+    {
+        if ( order > (*this_).order[pos] )
+        {
+            insert_pos = pos+1;
+        }
+        else if ( order == (*this_).order[pos] )
+        {
+            duplicate = true;
+            TRACE_INFO_INT( "duplicate:", order );
+        }
+    }
+
+    /* insert if possible */
+    if ( ! duplicate )
+    {
+        if ( (*this_).num_points < GEOMETRY_NON_LINEAR_SCALE_MAX_POINTS )
+        {
+            double lower_bound;
+            double upper_bound;
+            lower_bound = (*this_).location[ 0 ];
+            upper_bound = (*this_).location[ (*this_).num_points - 1 ];
+
+            /* insert order value */
+            for ( uint32_t pos2 = (*this_).num_points; pos2 > insert_pos; pos2 -- )
+            {
+                (*this_).order[ pos2 ] = (*this_).order[ pos2-1 ];
+            }
+            (*this_).order[ insert_pos ] = order;
+            (*this_).num_points ++;
+
+            /* re-location in interval */
+            double interval_width;
+            interval_width = ( upper_bound - lower_bound );
+            for ( uint32_t pos3 = 0; pos3 < (*this_).num_points; pos3 ++ )
+            {
+                (*this_).location[ pos3 ] = lower_bound + ( (double) pos3 * interval_width ) / (double) ( (*this_).num_points - 1 );
+            }
+        }
+        else
+        {
+            LOG_WARNING( "geometry_non_linear_scale_t has not enough points." );
+        }
+    }
 
     TRACE_END();
 }
