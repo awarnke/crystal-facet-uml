d8464f30d9830930df795cb0216f9ae1a0a6b532
A. Warnke <andreas.warnke@gmx.net>
2017-01-24 21:37:57 +0100

    cleaning up diagram_maker and diagram_painter classes

--- pencil/source/pencil_diagram_painter.c
+++ pencil/source/pencil_diagram_painter.c
@@ -42,7 +42,94 @@ void pencil_diagram_painter_draw ( pencil_diagram_painter_t *this_,
     assert( NULL != cr );
     assert( NULL != diagram_bounds );
 
-    TSLOG_ERROR("not yet implemented");
+    double left = geometry_rectangle_get_left ( diagram_bounds );
+    double top = geometry_rectangle_get_top ( diagram_bounds );
+    double right = geometry_rectangle_get_right ( diagram_bounds );
+    double bottom = geometry_rectangle_get_bottom ( diagram_bounds );
+    double width = geometry_rectangle_get_width ( diagram_bounds );
+    double height = geometry_rectangle_get_height ( diagram_bounds );
+
+    TRACE_INFO_INT( "w", (int)(width) );
+    TRACE_INFO_INT( "h", (int)(height) );
+
+    double gap = pencil_size_get_standard_object_border( pencil_size );
+    double f_line_gap = pencil_size_get_font_line_gap( pencil_size );
+    double f_tab_size = pencil_size_get_font_tab_size( pencil_size );
+
+    /* draw diagram border and name */
+    {
+        TRACE_INFO_INT("drawing diagram id",data_diagram_get_id(the_diagram));
+
+        double std_line_width = pencil_size_get_standard_line_width( pencil_size );
+        cairo_set_line_width( cr, std_line_width );
+        if ( data_diagram_is_valid(the_diagram) )
+        {
+            /* draw border line */
+            if ( mark_highlighted )
+            {
+                GdkRGBA highlight_color;
+                highlight_color = pencil_size_get_highlight_color( pencil_size );
+                cairo_set_source_rgba( cr, highlight_color.red, highlight_color.green, highlight_color.blue, highlight_color.alpha );
+            }
+            else
+            {
+                GdkRGBA standard_color;
+                standard_color = pencil_size_get_standard_color( pencil_size );
+                cairo_set_source_rgba( cr, standard_color.red, standard_color.green, standard_color.blue, standard_color.alpha );
+            }
+            cairo_rectangle ( cr, left+gap, top+gap, width-2.0*gap, height-2.0*gap );
+            cairo_stroke (cr);
+
+            /* draw title corner */
+            int text_width;
+            int text_height;
+            pango_layout_set_font_description (layout, pencil_size_get_standard_font_description(pencil_size) );
+            pango_layout_set_text (layout, data_diagram_get_name_ptr( the_diagram ), -1);
+            pango_layout_get_pixel_size (layout, &text_width, &text_height);
+            cairo_move_to ( cr, left + gap + f_tab_size, top+gap );
+            pango_cairo_show_layout (cr, layout);
+
+            static const double EDGE_45 = 4.0;
+            double title_corner_width = text_width + gap + 2.0*f_tab_size + EDGE_45;
+            if ( title_corner_width > width*0.9 )
+            {
+                title_corner_width = width*0.9;
+            }
+            cairo_move_to ( cr, left+gap, top+gap+text_height+f_line_gap );
+            cairo_line_to ( cr, left+title_corner_width - EDGE_45, top+gap+text_height+f_line_gap );
+            cairo_line_to ( cr, left+title_corner_width, top+gap+text_height+f_line_gap-EDGE_45 );
+            cairo_line_to ( cr, left+title_corner_width, top+gap );
+            cairo_stroke (cr);
+        }
+        else
+        {
+            /* draw cross line */
+            GdkRGBA standard_color;
+            standard_color = pencil_size_get_standard_color( pencil_size );
+            cairo_set_source_rgba( cr, standard_color.red, standard_color.green, standard_color.blue, standard_color.alpha );
+            cairo_move_to ( cr, left, top );
+            cairo_line_to ( cr, right, bottom );
+            cairo_move_to ( cr, left, bottom );
+            cairo_line_to ( cr, right, top );
+            cairo_stroke (cr);
+        }
+    }
+
+    /* mark focused and highlighted */
+    {
+        if ( mark_focused )
+        {
+            pencil_marker_mark_focused_rectangle( &((*this_).marker), *diagram_bounds, cr );
+        }
+
+        if ( mark_selected )
+        {
+            geometry_rectangle_t selected_rect;
+            geometry_rectangle_init( &selected_rect, left+gap, top+gap, width-2.0*gap, height-2.0*gap );
+            pencil_marker_mark_selected_rectangle( &((*this_).marker), selected_rect, cr );
+            geometry_rectangle_destroy( &selected_rect );
+        }
+    }
 
     TRACE_END();
 }
