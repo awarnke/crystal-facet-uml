46aacc4f2c90277cbb94873992e654b128fb3c4e
A. Warnke <andreas.warnke@gmx.net>
2017-05-01 22:10:47 +0200

    refman.pdf is generated from doxygen comments

--- doxygen_build/make.sh
+++ doxygen_build/make.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 doxygen doxygen_config
+echo "check that you have the latex package pdflatex installed."
+echo "check that you have the latex package xtab installed."
+echo "check that you have the latex package multirow installed."
+echo "check that you have the latex package sectsty installed."
+echo "check that you have the latex package tocloft installed."
+cd doc/latex
+make
