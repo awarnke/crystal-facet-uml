e767a4239d564b30e7c9c6cb56a6f46e451e3909
A. Warnke <andreas.warnke@gmx.net>
2018-11-19 21:29:34 +0100

    debian packaging prerequisites for new version

--- package_build/debian_config/debian/crystal-facet-uml.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the crystal-facet-uml package
-#
-0 4	* * *	root	[ -x /usr/bin/crystal-facet-uml_maintenance ] && /usr/bin/crystal-facet-uml_maintenance
--- package_build/debian_config/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(crystal-facet-uml):needs="X11|text|vc|wm" section="Applications/Science/Engineering"\
-  title="crystal-facet-uml" command="/usr/bin/crystal-facet-uml"
