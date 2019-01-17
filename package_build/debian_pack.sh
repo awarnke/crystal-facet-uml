ebd70a2e6e87415be5ad74dfbf2036f8e4f530a0
A. Warnke <andreas.warnke@gmx.net>
2019-01-17 20:22:16 +0100

    lessons learned from packaging 1.9.0 written to comments

--- package_build/debian_pack.sh
+++ package_build/debian_pack.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "1.) Update the VERSIONSTR in this debian_pack.sh script"
+echo "2.) Copy the latest entry in debian_config/debian/changelog"
+echo "3.) cd debian_config && dch -i && vi debian/changelog"
 VERSIONSTR=1.9.0
 echo "clean old files and directories"
 test -e crystal-facet-uml_$VERSIONSTR.orig.tar.gz && rm crystal-facet-uml_$VERSIONSTR.orig.tar.gz
