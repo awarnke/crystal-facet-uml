e13a7a1d5020e39dcd644947ef1b3b9acfbdaa82
Andreas Warnke <5134-awarnke-guest@users.noreply.salsa.debian.org>
2019-09-02 20:36:47 +0200

    first mingw compiled 10027724 Sep  2 20:34 crystal_facet_uml.exe

--- build_win/mingw_wine_toolchain.cmake
+++ build_win/mingw_wine_toolchain.cmake
@@ -4,11 +4,11 @@
 SET(CMAKE_C_COMPILER "/usr/bin/i686-w64-mingw32-gcc")
 SET(CMAKE_CXX_COMPILER "/usr/bin/i686-w64-mingw32-g++")
 SET(CMAKE_SYSTEM_NAME "Windows")
-include(CMakeForceCompiler)
-IF("${GNU_HOST}" STREQUAL "")
-    SET(GNU_HOST i686-w64-mingw32)
-ENDIF()
+#include(CMakeForceCompiler)
+#IF("${GNU_HOST}" STREQUAL "")
+#    SET(GNU_HOST i686-w64-mingw32)
+#ENDIF()
 # Prefix detection only works with compiler id "GNU"
-CMAKE_FORCE_C_COMPILER(i686-w64-mingw32-gcc GNU)
+#CMAKE_FORCE_C_COMPILER(i686-w64-mingw32-gcc GNU)
 # manually define 'windres':
-SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(CMAKE_RC_COMPILER "/usr/bin/i686-w64-mingw32-windres")
