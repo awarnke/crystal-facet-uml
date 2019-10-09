dd9b43e4b8c8f6cf8af9a2e75ed7a5bb3f79a145
Andreas Warnke <cfu@andreaswarnke.de>
2019-10-09 21:29:01 +0200

    gtk rectangle parameters corrected

--- gui/source/sketch_area/gui_sketch_background.c
+++ gui/source/sketch_area/gui_sketch_background.c
@@ -71,21 +71,24 @@ void gui_sketch_background_draw_introduction( gui_sketch_background_t *this_,
 
     int text_area_start;
 
+    /* if there is enough space, draw a nice picture bar on the left side */
     if ( width > 192 )
     {
-        /* if there is enough space, draw a nice dark bar on the left side */
-        text_area_start = left+96;
-
-        //cairo_set_source_rgba( cr, DARK_R, DARK_G, DARK_B, DARK_A );
-        //cairo_rectangle ( cr, left, top, left+96, top+height );
-        //cairo_fill (cr);
-
         GdkPixbuf *bg_img = gui_resources_get_background_column( (*this_).resources );
-        //double icon_width = gdk_pixbuf_get_width ( bg_img );
-        //double icon_height = gdk_pixbuf_get_height ( bg_img );
+        double icon_width = gdk_pixbuf_get_width ( bg_img );
+        double icon_height = gdk_pixbuf_get_height ( bg_img );
         gdk_cairo_set_source_pixbuf( cr, bg_img, left, top );
-        cairo_rectangle ( cr, left, top, left+96, top+height );
+        cairo_rectangle ( cr, left, top, icon_width, height );
         cairo_fill (cr);
+
+        text_area_start = left+icon_width;
+
+        if ( height > icon_height ) 
+        {
+            cairo_set_source_rgba( cr, DARK_R, DARK_G, DARK_B, DARK_A );
+            cairo_rectangle ( cr, left, top+icon_height, icon_width, height-icon_height );
+            cairo_fill (cr);
+        }
     }
     else
     {
