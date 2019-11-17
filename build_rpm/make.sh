21fe5dcc4c1aa8fa3158e65cb3882cd4c422c2dd
A. Warnke <andreas.warnke@gmx.net>
2019-11-17 09:56:20 +0100

    rpm package generation script added

---
+++ build_rpm/make.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+VERSIONSTR=1.16.0
+
+echo "prepare sources"
+test -e SOURCES && rm -fr SOURCES
+mkdir SOURCES
+cd ..
+git archive --format tar.gz --prefix=crystal-facet-uml-$VERSIONSTR/ --output=build_rpm/SOURCES/crystal-facet-uml_$VERSIONSTR.orig.tar.gz master
+cd build_rpm
+
+echo "prepare signatures"
+echo "%_signature gpg
+%_gpg_path /home/andi/.gnupg
+%_gpg_name Andreas Warnke
+%_gpgbin /usr/bin/gpg" > ~/.rpmmacros
+
+echo "do build"
+test -e BUILD && rm -fr BUILD
+rpmbuild --define "_topdir `pwd`" -bb SPECS/crystal-facet-uml.spec --sign
