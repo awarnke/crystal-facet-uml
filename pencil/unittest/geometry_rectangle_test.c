d36e9e1996740a7a6283cdc8a54b840a04db686a
Andreas Warnke <cfu@andreaswarnke.de>
2020-05-24 08:18:24 +0200

    test cases for geometry_rectangle_init_by_difference completed

--- pencil/unittest/geometry_rectangle_test.c
+++ pencil/unittest/geometry_rectangle_test.c
@@ -12,7 +12,8 @@ static void test_bounds(void);
 static void test_difference_basic(void);
 static void test_difference_4_candidates(void);
 static void test_difference_3_candidates(void);
-static void test_difference_2_candidates(void);
+static void test_difference_2_corner_candidates(void);
+static void test_difference_2_stripe_candidates(void);
 static void test_difference_1_candidate(void);
 
 test_suite_t geometry_rectangle_test_get_list(void)
@@ -25,7 +26,8 @@ test_suite_t geometry_rectangle_test_get_list(void)
     test_suite_add_test_case( &result, "test_difference_basic", &test_difference_basic );
     test_suite_add_test_case( &result, "test_difference_4_candidates", &test_difference_4_candidates );
     test_suite_add_test_case( &result, "test_difference_3_candidates", &test_difference_3_candidates );
-    test_suite_add_test_case( &result, "test_difference_2_candidates", &test_difference_2_candidates );
+    test_suite_add_test_case( &result, "test_difference_2_corner_candidates", &test_difference_2_corner_candidates );
+    test_suite_add_test_case( &result, "test_difference_2_stripe_candidates", &test_difference_2_stripe_candidates );
     test_suite_add_test_case( &result, "test_difference_1_candidate", &test_difference_1_candidate );
     return result;
 }
@@ -225,9 +227,9 @@ static void test_difference_4_candidates(void)
         geometry_rectangle_init ( &rect_a, 0.0, 0.0, 5.0 /*width*/, 5.0 /*height*/ );
         geometry_rectangle_init ( &rect_b, in_x[case_idx], in_y[case_idx], 1.0 /*width*/, 1.0 /*height*/ );
         geometry_rectangle_init_by_difference( &diff_rect, &rect_a, &rect_b );
-        geometry_rectangle_trace( &rect_a );
-        geometry_rectangle_trace( &rect_b );
-        geometry_rectangle_trace( &diff_rect );
+        //geometry_rectangle_trace( &rect_a );
+        //geometry_rectangle_trace( &rect_b );
+        //geometry_rectangle_trace( &diff_rect );
         TEST_ASSERT( geometry_rectangle_is_containing( &rect_a, &diff_rect ) );
         TEST_ASSERT_EQUAL_DOUBLE( 0.0, geometry_rectangle_get_intersect_area( &rect_b, &diff_rect ) );
         TEST_ASSERT_EQUAL_DOUBLE( 15.0, geometry_rectangle_get_area( &diff_rect ) );
@@ -243,7 +245,7 @@ static void test_difference_3_candidates(void)
     geometry_rectangle_t rect_b;
     geometry_rectangle_t diff_rect;
 
-    /*                  from left      from top       from right     from bottom    */
+    /*                  from left      from bottom    from right     from top     */
     double in_x[12] = { 0.0, 0.0, 0.0, 2.0, 4.5, 7.0, 2.0, 7.0, 2.0, 2.0, 4.5, 7.0 };
     double in_y[12] = { 2.0, 4.5, 7.0, 2.0, 7.0, 2.0, 2.0, 4.5, 7.0, 0.0, 0.0, 0.0 };
     double in_w[12] = { 8.0, 3.0, 8.0, 1.0, 1.0, 1.0, 8.0, 3.0, 8.0, 1.0, 1.0, 1.0 };
@@ -262,25 +264,61 @@ static void test_difference_3_candidates(void)
     }
 }
 
-static void test_difference_2_candidates(void)
+/*   0  1  2  3  4  5  6  7  8  9  10  */
+/*  0  .  .  .  .  .  .  .  .  .  .    */
+/*  1  .  .  .  .  .  .  .  .  .  .    */
+/*  2  .  .  .  .  .  .  .  .  .  .    */
+/*  3  .  .  .  _  _  _  _  .  .  .    */
+/*  4  .  .  .  _  _  _  _  .  .  .    */
+/*  5  .  .  .  _  _  _  _  .  .  .    */
+/*  6  .  .  .  _  _  _  _  .  .  .    */
+/*  7  .  .  .  .  .  .  .  .  .  .    */
+/*  8  .  .  .  .  .  .  .  .  .  .    */
+/*  9  .  .  .  .  .  .  .  .  .  .    */
+/* 10                                  */
+
+static void test_difference_2_corner_candidates(void)
 {
     geometry_rectangle_t rect_a;
     geometry_rectangle_t rect_b;
     geometry_rectangle_t diff_rect;
 
-    /*                  from left      from top       from right     from bottom    */
-    double in_x[12] = { 0.0, 0.0, 0.0, 2.0, 4.5, 7.0, 2.0, 7.0, 2.0, 2.0, 4.5, 7.0 };
-    double in_y[12] = { 2.0, 4.5, 7.0, 2.0, 7.0, 2.0, 2.0, 4.5, 7.0, 0.0, 0.0, 0.0 };
-    double in_w[12] = { 8.0, 3.0, 8.0, 1.0, 1.0, 1.0, 8.0, 3.0, 8.0, 1.0, 1.0, 1.0 };
-    double in_h[12] = { 1.0, 1.0, 1.0, 8.0, 3.0, 8.0, 1.0, 1.0, 1.0, 8.0, 3.0, 8.0 };
+    /*                 le.to.    ri.to.    ri.bo.    le.bo.  */
+    double in_x[8] = { 2.0, 1.0, 5.0, 6.0, 6.0, 5.0, 6.0, 1.0 };
+    double in_y[8] = { 1.0, 2.0, 1.0, 2.0, 5.0, 6.0, 2.0, 5.0 };
     for ( int case_idx = 0; case_idx < 8; case_idx ++ )
     {
-        geometry_rectangle_init ( &rect_a, 1.0, 1.0, 8.0 /*width*/, 8.0 /*height*/ );
+        geometry_rectangle_init ( &rect_a, 3.0, 3.0, 4.0 /*width*/, 4.0 /*height*/ );
+        geometry_rectangle_init ( &rect_b, in_x[case_idx], in_y[case_idx], 3.0, 3.0 );
+        geometry_rectangle_init_by_difference( &diff_rect, &rect_a, &rect_b );
+        TEST_ASSERT( geometry_rectangle_is_containing( &rect_a, &diff_rect ) );
+        TEST_ASSERT_EQUAL_DOUBLE( 0.0, geometry_rectangle_get_intersect_area( &rect_b, &diff_rect ) );
+        TEST_ASSERT_EQUAL_DOUBLE( 12.0, geometry_rectangle_get_area( &diff_rect ) );
+        geometry_rectangle_destroy ( &rect_a );
+        geometry_rectangle_destroy ( &rect_b );
+        geometry_rectangle_destroy ( &diff_rect );
+    }
+}
+
+static void test_difference_2_stripe_candidates(void)
+{
+    geometry_rectangle_t rect_a;
+    geometry_rectangle_t rect_b;
+    geometry_rectangle_t diff_rect;
+
+    /*                 vertical  horizontal */
+    double in_x[4] = { 4.0, 5.0, 1.0, 1.0 };
+    double in_y[4] = { 1.0, 1.0, 4.0, 5.0 };
+    double in_w[4] = { 1.0, 1.0, 8.0, 8.0 };
+    double in_h[4] = { 8.0, 8.0, 1.0, 1.0 };
+    for ( int case_idx = 0; case_idx < 4; case_idx ++ )
+    {
+        geometry_rectangle_init ( &rect_a, 3.0, 3.0, 4.0 /*width*/, 4.0 /*height*/ );
         geometry_rectangle_init ( &rect_b, in_x[case_idx], in_y[case_idx], in_w[case_idx], in_h[case_idx] );
         geometry_rectangle_init_by_difference( &diff_rect, &rect_a, &rect_b );
         TEST_ASSERT( geometry_rectangle_is_containing( &rect_a, &diff_rect ) );
         TEST_ASSERT_EQUAL_DOUBLE( 0.0, geometry_rectangle_get_intersect_area( &rect_b, &diff_rect ) );
-        TEST_ASSERT_EQUAL_DOUBLE( 48.0, geometry_rectangle_get_area( &diff_rect ) );
+        TEST_ASSERT_EQUAL_DOUBLE( 8.0, geometry_rectangle_get_area( &diff_rect ) );
         geometry_rectangle_destroy ( &rect_a );
         geometry_rectangle_destroy ( &rect_b );
         geometry_rectangle_destroy ( &diff_rect );
@@ -308,6 +346,7 @@ static void test_difference_1_candidate(void)
         geometry_rectangle_destroy ( &diff_rect );
     }
 }
+
 
 /*
  * Copyright 2017-2020 Andreas Warnke
