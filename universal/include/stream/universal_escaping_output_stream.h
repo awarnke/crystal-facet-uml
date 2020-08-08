19389d10e5c4a4920f631c05d6031eceeac923b2
Andreas Warnke <cfu@andreaswarnke.de>
2020-08-08 18:19:05 +0200

    header for escaping utf8-writer declared

---
+++ universal/include/stream/universal_escaping_output_stream.h
@@ -0,0 +1,111 @@
+/* File: universal_escaping_output_stream.h; Copyright and License: see below */
+
+#ifndef UNIVERSAL_ESCAPING_OUTPUT_STREAM_H
+#define UNIVERSAL_ESCAPING_OUTPUT_STREAM_H
+
+/* public file for the doxygen documentation: */
+/*!
+ *  \file
+ *  \brief implements an universal_output_stream_if that replaces a set of predefined
+ *         character sequences by other character sequences and forwards the output
+ *         to an other output stream.
+ *
+ *  This class can be used to e.g escape a couple of characters by escape sequences
+ *  or to indent lines after every newline character.
+ *  This class does not search for sequences that are split between multiple write calls.
+ */
+
+#include "stream/universal_output_stream.h"
+
+/*!
+ *  \brief attributes of the universal_escaping_output_stream
+ */
+struct universal_escaping_output_stream_struct {
+    universal_output_stream_t output_stream;  /*!< instance of implemented interface \c universal_output_stream_t */
+    const char *const ((*patterns_and_replacements)[][2]);  /*!< array of 0-terminated pattern and replacement strings, NULL terminated. */
+    universal_output_stream_t *sink;  /*!< pointer to data stream sink */
+};
+
+typedef struct universal_escaping_output_stream_struct universal_escaping_output_stream_t;
+
+/*!
+ *  \brief initializes the universal_escaping_output_stream_t
+ *
+ *  \param this_ pointer to own object attributes
+ *  \param patterns_and_replacements pointer to 2-dim array, NULL-terminated,
+ *         each arrray-entry is a pointer to a 0-terminated string.
+ *  \param pointer to data stream sink
+ */
+void universal_escaping_output_stream_init( universal_escaping_output_stream_t *this_,
+                                            const char *const ((*patterns_and_replacements)[][2]),
+                                            universal_output_stream_t *sink
+                                        );
+
+/*!
+ *  \brief destroys the universal_escaping_output_stream_t
+ *
+ *  \param this_ pointer to own object attributes
+ *  \result 0 in case of success, -1 otherwise
+ */
+int universal_escaping_output_stream_destroy( universal_escaping_output_stream_t *this_ );
+
+/*!
+ *  \brief opens a stream
+ *
+ *  \param this_ pointer to own object attributes
+ *  \param path resource path identifying the stream to open for writing
+ *  \result 0 in case of success, -1 otherwise
+ */
+int universal_escaping_output_stream_open ( universal_escaping_output_stream_t *this_, const char *path );
+
+/*!
+ *  \brief writes a buffer (e.g. a stringview) to the data sink, replacing patterns by replacements
+ *
+ *  \param this_ pointer to own object attributes
+ *  \param start buffer to write, not 0-terminated
+ *  \param length length of the buffer to write
+ *  \result 0 in case of success, -1 otherwise
+ */
+int universal_escaping_output_stream_write ( universal_escaping_output_stream_t *this_, const void *start, size_t length );
+
+/*!
+ *  \brief flushes buffers
+ *
+ *  \param this_ pointer to own object attributes
+ *  \result 0 in case of success, -1 otherwise
+ */
+int universal_escaping_output_stream_flush( universal_escaping_output_stream_t *this_ );
+
+/*!
+ *  \brief closes the universal_escaping_output_stream_t
+ *
+ *  \param this_ pointer to own object attributes
+ *  \result 0 in case of success, -1 otherwise
+ */
+int universal_escaping_output_stream_close( universal_escaping_output_stream_t *this_ );
+
+/*!
+ *  \brief gets the output stream interface of this universal_escaping_output_stream_t
+ *
+ *  \param this_ pointer to own object attributes
+ */
+universal_output_stream_t* universal_escaping_output_stream_get_output_stream( universal_escaping_output_stream_t *this_ );
+
+#endif  /* UNIVERSAL_ESCAPING_OUTPUT_STREAM_H */
+
+
+/*
+Copyright 2020-2020 Andreas Warnke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
---
+++ universal/source/stream/universal_escaping_output_stream.c
@@ -0,0 +1,119 @@
+/* File: universal_escaping_output_stream.c; Copyright and License: see below */
+
+#include "stream/universal_escaping_output_stream.h"
+#include "stream/universal_output_stream_if.h"
+#include "trace.h"
+#include "tslog.h"
+#include <string.h>
+#include <assert.h>
+
+/* the vmt implementing the interface */
+static const universal_output_stream_if_t universal_escaping_output_stream_private_if
+    = {
+        /* open:   */ (int (*)(void*, const char*)) &universal_escaping_output_stream_open,
+        /* write:  */ (int (*)(void*, const void*, size_t)) &universal_escaping_output_stream_write,
+        /* flush:  */ (int (*)(void*)) &universal_escaping_output_stream_flush,
+        /* close:  */ (int (*)(void*)) &universal_escaping_output_stream_close,
+        /* destroy:*/ (int (*)(void*)) &universal_escaping_output_stream_destroy
+    };
+
+void universal_escaping_output_stream_init ( universal_escaping_output_stream_t *this_,
+                                             const char *const ((*patterns_and_replacements)[][2]),
+                                             universal_output_stream_t *sink )
+{
+    TRACE_BEGIN();
+    assert( patterns_and_replacements != NULL );
+    assert( sink != NULL );
+
+    (*this_).patterns_and_replacements = patterns_and_replacements;
+    (*this_).sink = sink;
+    universal_output_stream_init( &((*this_).output_stream), &universal_escaping_output_stream_private_if, this_ );
+
+    TRACE_END();
+}
+
+int universal_escaping_output_stream_destroy( universal_escaping_output_stream_t *this_ )
+{
+    TRACE_BEGIN();
+    int err = 0;
+
+    (*this_).patterns_and_replacements = NULL;
+    (*this_).sink = NULL;
+    /* do not call universal_output_stream_destroy here - this would be a recursion */
+
+    TRACE_END_ERR(err);
+    return err;
+}
+
+int universal_escaping_output_stream_open ( universal_escaping_output_stream_t *this_, const char *path )
+{
+    TRACE_BEGIN();
+    assert( (*this_).sink != NULL );
+
+    const int err = universal_output_stream_open( (*this_).sink, path );
+
+    TRACE_END_ERR(err);
+    return err;
+}
+
+int universal_escaping_output_stream_write ( universal_escaping_output_stream_t *this_, const void *start, size_t length )
+{
+    TRACE_BEGIN();
+    assert( start != NULL );
+    assert( (*this_).patterns_and_replacements != NULL );
+    assert( (*this_).sink != NULL );
+
+    const int err = universal_output_stream_write( (*this_).sink, start, length );
+
+    TRACE_END_ERR(err);
+    return err;
+}
+
+int universal_escaping_output_stream_flush( universal_escaping_output_stream_t *this_ )
+{
+    TRACE_BEGIN();
+    assert( (*this_).sink != NULL );
+
+    const int err = universal_output_stream_flush( (*this_).sink );
+
+    TRACE_END_ERR(err);
+    return err;
+}
+
+int universal_escaping_output_stream_close( universal_escaping_output_stream_t *this_ )
+{
+    TRACE_BEGIN();
+    assert( (*this_).sink != NULL );
+
+    const int err = universal_output_stream_close( (*this_).sink );
+
+    TRACE_END_ERR(err);
+    return err;
+}
+
+universal_output_stream_t* universal_escaping_output_stream_get_output_stream( universal_escaping_output_stream_t *this_ )
+{
+    TRACE_BEGIN();
+
+    universal_output_stream_t* result = &((*this_).output_stream);
+
+    TRACE_END();
+    return result;
+}
+
+
+/*
+Copyright 2020-2020 Andreas Warnke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
