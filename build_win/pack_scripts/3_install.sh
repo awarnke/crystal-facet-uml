160c3ecd43d5a46949bcc1a566fbb28bf34b637c
A. Warnke <andreas.warnke@gmx.net>
2020-12-19 14:19:49 +0100

    program renamed from crystal_facet_uml to crystal-facet-uml, fix for windows build

--- build_win/pack_scripts/3_install.sh
+++ build_win/pack_scripts/3_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mv unittest_crystal-facet-uml.exe crystal-facet-uml/bin/
-mv crystal-facet-uml crystal-facet-uml
+mv crystal-facet-uml.exe crystal-facet-uml/bin/
 cp ../../installation_win/crystal-facet-uml.bat crystal-facet-uml
 cp ../../installation_win/crystal-facet-uml_readme.txt crystal-facet-uml
 cp ../../example_diagrams/mouse_droid.cfu1 crystal-facet-uml/crystal-facet-uml_mod5g-example.cfu1
