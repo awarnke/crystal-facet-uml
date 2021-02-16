b61faebf4c95de41df7582f703f0e223ebf2ee55
Andreas Warnke <cfu@andreaswarnke.de>
2021-02-16 21:57:42 +0100

    folder resturcturing fixed for mingw win cross compile

--- build/win/pack_scripts/3_install.sh
+++ build/win/pack_scripts/3_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mv test_crystal-facet-uml.exe crystal-facet-uml/bin/
 mv crystal-facet-uml.exe crystal-facet-uml/bin/
-cp ../../installation_win/crystal-facet-uml.bat crystal-facet-uml
-cp ../../installation_win/crystal-facet-uml_readme.txt crystal-facet-uml
-cp ../../example_diagrams/mouse_droid.cfu1 crystal-facet-uml/crystal-facet-uml_mod5g-example.cfu1
-cp ../../user_doc/crystal-facet-uml_documentation.pdf crystal-facet-uml
+cp ../../../installation_win/crystal-facet-uml.bat crystal-facet-uml
+cp ../../../installation_win/crystal-facet-uml_readme.txt crystal-facet-uml
+cp ../../../example_diagrams/mouse_droid.cfu1 crystal-facet-uml/crystal-facet-uml_mod5g-example.cfu1
+cp ../../../user_doc/crystal-facet-uml_documentation.pdf crystal-facet-uml
