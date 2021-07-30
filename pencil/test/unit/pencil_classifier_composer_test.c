90b57f9c4716e629c332ad486eb5b1a0a1ffa491
A. Warnke <andreas.warnke@gmx.net>
2021-07-30 15:55:01 +0200

    version 1.31.0 test case test_expand_inner_space modified 2nd time

--- pencil/test/unit/pencil_classifier_composer_test.c
+++ pencil/test/unit/pencil_classifier_composer_test.c
@@ -112,7 +112,7 @@ static void test_expand_inner_space(void)
     pencil_classifier_composer_t classifier_composer;
     pencil_classifier_composer_init( &classifier_composer );
 
-    const geometry_rectangle_t inner_space = { .left = 100.0, .top = 90.0, .width = 260.0, .height = 180.0 };
+    const geometry_rectangle_t inner_space = { .left = 100.0, .top = 90.0, .width = 210.0, .height = 60.0 };
 
     for ( unsigned int t_idx = 0; t_idx < DATA_CLASSIFIER_TYPE_COUNT; t_idx ++ )
     {
@@ -145,8 +145,8 @@ static void test_expand_inner_space(void)
             const geometry_rectangle_t *const space = layout_visible_classifier_get_space_const( &layout_vis_classifier );
             TEST_ASSERT_EQUAL_DOUBLE( 100.0, geometry_rectangle_get_left( space ) );
             TEST_ASSERT_EQUAL_DOUBLE( 90.0, geometry_rectangle_get_top( space ) );
-            TEST_ASSERT_EQUAL_DOUBLE( 260.0, geometry_rectangle_get_width( space ) );
-            TEST_ASSERT_EQUAL_DOUBLE( 180.0, geometry_rectangle_get_height( space ) );
+            TEST_ASSERT_EQUAL_DOUBLE( 210.0, geometry_rectangle_get_width( space ) );
+            TEST_ASSERT_EQUAL_DOUBLE( 60.0, geometry_rectangle_get_height( space ) );
         }
     }
 
@@ -158,7 +158,7 @@ static void test_set_envelope_box(void)
     pencil_classifier_composer_t classifier_composer;
     pencil_classifier_composer_init( &classifier_composer );
 
-    const geometry_rectangle_t envelope = { .left = 50.0, .top = 40.0, .width = 210.0, .height = 180.0 };
+    const geometry_rectangle_t envelope = { .left = 50.0, .top = 40.0, .width = 260.0, .height = 180.0 };
 
     for ( unsigned int t_idx = 0; t_idx < DATA_CLASSIFIER_TYPE_COUNT; t_idx ++ )
     {
