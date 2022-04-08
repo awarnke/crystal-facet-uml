e083464ef40c89589e673b1f2be0d08b21056c86
A. Warnke <andreas.warnke@gmx.net>
2022-04-08 17:03:29 +0200

    startup bat for windows adapted

--- installation_win/crystal-facet-uml.bat
+++ installation_win/crystal-facet-uml.bat
@@ -1,3 +1,4 @@
-@set XDG_DATA_DIR="%CD%\share"
-@set XDG_DATA_DIRS="%CD%\share"
-@bin\crystal-facet-uml.exe %1 %2 %3 %4
+@echo set XDG_DATA_DIR="%CD%\share"
+@echo set XDG_DATA_DIRS="%CD%\share\glib-2.0\schemas"
+@cd share\glib-2.0\schemas
+@..\..\..\bin\crystal-facet-uml.exe %1 %2 %3 %4
