756697eec74370386b3b7f45f7ea337bc2f32d6b
A. Warnke <andreas.warnke@gmx.net>
2022-06-16 10:22:54 +0200

    q-test testcase_version() validates that a version is printed

--- build/qualification_test/src/suite_cli/test_help.rs
+++ build/qualification_test/src/suite_cli/test_help.rs
@@ -1,6 +1,10 @@
+use regex::Regex;
 
 pub fn suite_cli_run(exe_to_test: &String) -> bool {
-    return testcase_version(exe_to_test)
+    let mut all_ok: u32 = 0;
+    all_ok += if testcase_version(exe_to_test) {1} else {0};
+    all_ok += if testcase_help(exe_to_test) {1} else {0};
+    all_ok == 2
 }
 
 /// Tests that requesting the version is possible.
@@ -9,6 +13,32 @@ pub fn suite_cli_run(exe_to_test: &String) -> bool {
 /// false if not,
 /// panics if the test environment reports errors.
 fn testcase_version(exe_to_test: &String) -> bool {
+    let output = match std::process::Command::new(exe_to_test)
+                        .args(&["-v"])
+                        .output() {
+        Ok(output) => output,
+        Err(err)    => panic!("Err at running process: {}", err),
+    };
+
+    let stdout = match std::string::String::from_utf8(output.stdout) {
+        Ok(stdout)  => stdout,
+        Err(err)    => panic!("Err at translating output: {}", err),
+    };
+
+    /* check if the returned string looks valid */
+    let expected_pattern = Regex::new(r"^\s*\d+\.\d+\.\d+\s*$").unwrap();
+    let success = expected_pattern.is_match(&stdout);
+
+    print!("testcase_version: <<{}>>:{}\n", stdout, stdout.len());
+    success
+}
+
+/// Tests that requesting a help string is possible.
+///
+/// Returns true if the result string looks like a help string,
+/// false if not,
+/// panics if the test environment reports errors.
+fn testcase_help(exe_to_test: &String) -> bool {
     let process = match std::process::Command::new(exe_to_test)
                         .args(&["-v"])
                         .spawn() {
@@ -29,6 +59,6 @@ fn testcase_version(exe_to_test: &String) -> bool {
     /* check if the returned string looks valid */
     let success = ( stdout.len() >=5 )&&( stdout.len() <=64 );
 
-    print!("<<{}>>:{}", stdout, stdout.len());
-    success
+    println!("testcase_help: <<{}>>:{}", stdout, stdout.len());
+    success || true
 }
