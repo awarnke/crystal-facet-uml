66703acebfed420bf28e0e8de9168c1554b9ff9a
A. Warnke <andreas.warnke@gmx.net>
2022-12-20 22:11:20 +0100

    suite_runner implements the TestSuite Trait

--- build/qualification_test/src/suite_cli/suite_runner.rs
+++ build/qualification_test/src/suite_cli/suite_runner.rs
@@ -2,8 +2,16 @@ use super::test_help::testcase_help;
 use super::test_help::testcase_version;
 use super::test_importexport::testcase_import_to_new_cfu1;
 use super::test_repair::testcase_repair_new_cfu1;
+use crate::test_tool::test_case::TestCase;
 use crate::test_tool::test_result::TestResult;
+use crate::test_tool::test_suite::TestSuite;
 use std::fs;
+
+pub struct SuiteCli<'a> {
+    pub name: &'static str,
+    exe_to_test: &'a str,
+    temp_dir: &'a str,
+}
 
 /// Runs all test cases in this test suite
 ///
@@ -20,29 +28,41 @@ pub fn suite_cli_run(exe_to_test: &str, temp_dir: &str) -> TestResult {
     result += testcase_version(exe_to_test);
     result += testcase_help(exe_to_test);
 
-    setup(temp_dir);
-    result += testcase_repair_new_cfu1(exe_to_test, temp_dir);
-    teardown(temp_dir);
+    let suite: &dyn TestSuite = &SuiteCli {
+        name: "SuiteCli",
+        exe_to_test: exe_to_test,
+        temp_dir: temp_dir,
+    };
 
-    setup(temp_dir);
+    suite.setup();
+    result += testcase_repair_new_cfu1(exe_to_test, temp_dir);
+    suite.teardown();
+
+    suite.setup();
     result += testcase_import_to_new_cfu1(exe_to_test, temp_dir);
-    teardown(temp_dir);
+    suite.teardown();
 
     result
 }
 
-/// Creates a test directory
-///
-/// Panics if the test environment reports errors.
-fn setup(temp_dir: &str) -> () {
-    /* create the temp directory, panic in case an error occured */
-    fs::create_dir_all(temp_dir).unwrap();
-}
+impl<'a> TestSuite for SuiteCli<'a> {
+    /// Creates a test directory
+    ///
+    /// Panics if the test environment reports errors.
+    fn setup(self: &Self) -> () {
+        /* create the temp directory, panic in case an error occured */
+        fs::create_dir_all(self.temp_dir).unwrap();
+    }
 
-/// Removes the test directory
-///
-/// Panics if the test environment reports errors.
-fn teardown(temp_dir: &str) -> () {
-    /* delete the temp directory in case of successful test result, panic in case an error occured */
-    fs::remove_dir_all(temp_dir).unwrap();
+    /// Removes the test directory
+    ///
+    /// Panics if the test environment reports errors.
+    fn teardown(self: &Self) -> () {
+        /* delete the temp directory in case of successful test result, panic in case an error occured */
+        fs::remove_dir_all(self.temp_dir).unwrap();
+    }
+
+    fn testcases(self: &Self) -> &'static [TestCase] {
+        &[]
+    }
 }
--- build/qualification_test/src/test_tool/test_case.rs
+++ build/qualification_test/src/test_tool/test_case.rs
@@ -1,3 +1,4 @@
 pub struct TestCase {
     pub name: &'static str,
+    pub run: fn() -> Result<(), ()>,
 }
