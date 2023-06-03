a1bd237aa8cbd5478b153e2911e44975fccf3d06
A. Warnke <andreas.warnke@gmx.net>
2023-06-03 08:54:16 +0200

    version 1.50.1 rc1

--- installation_win/crystal-facet-uml.bat
+++ installation_win/crystal-facet-uml.bat
@@ -2,5 +2,7 @@
 rem when calling crystal-facet-uml.bat, use absolute paths in parameters
 rem e.g. crystal-facet-uml.bat -u "%%CD%%\crystal-facet-uml_mod5g-example.cfu1"
 rem To not accidentially load a wrong library, reduce the path:
+set OLDPATH=%PATH%
 set PATH=bin
 start /B bin\crystal-facet-uml.exe %1 %2 %3 %4
+set PATH=%OLDPATH%
