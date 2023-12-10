4f9e180ffcd878ba481a2189d28723e5ff52c219
Andreas Warnke <cfu@andreaswarnke.de>
2023-12-10 13:06:35 +0100

    coverity parameters adapted

--- build/static_code_analysis/2_make.sh
+++ build/static_code_analysis/2_make.sh
@@ -3,6 +3,6 @@ SOURCE_ROOT=../..
 . ${SOURCE_ROOT}/main/include/meta/meta_version.inl
 VERSIONSTR=${META_VERSION_STR}
 [ -e Makefile ] && make clean
-cmake ${SOURCE_ROOT}
+cmake -DCFU_USE_GTK4=ON ${SOURCE_ROOT}
 cov-build --dir cov-int make -j4 crystal-facet-uml
 tar czvf crystal-facet-uml_${VERSIONSTR}.tgz cov-int
