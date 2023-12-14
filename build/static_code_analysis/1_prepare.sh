0bbd39896edfc5345eab98f9694e23f6c9268e4c
Andreas Warnke <cfu@andreaswarnke.de>
2023-12-14 17:51:37 +0100

    coverity updated to version 2023.6.2

--- build/static_code_analysis/1_prepare.sh
+++ build/static_code_analysis/1_prepare.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "You possibly need to remove the constant GLIB_DEPRECATED_ENUMERATOR_IN_2_26 from /usr/include/glib-2.0/gobject/gparam.h"
 echo "You possibly need to adapt the constant def of 2G_SPAWN_ERROR_2BIG2 in /usr/include/glib-2.0/glib/gspawn.h"
-export PATH=$PATH:~/carbondisk/bin/cov-analysis-linux64-2022.6.0/bin
+export PATH=$PATH:~/carbondisk/bin/cov-analysis-linux64-2023.6.2/bin
