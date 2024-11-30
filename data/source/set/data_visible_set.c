14788c9842baefa172af680fba9a44783e962caf
Andreas Warnke <andreas.warnke@gmx.net>
2024-11-30 20:27:32 +0100

    diagrams can show the defined number of features and relationships     independant of number of other diagrams

--- data/source/set/data_visible_set.c
+++ data/source/set/data_visible_set.c
@@ -1,6 +1,7 @@
 /* File: data_visible_set.c; Copyright and License: see below */
 
 #include "set/data_visible_set.h"
+#include "data_rules.h"
 #include "u8/u8_trace.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -66,79 +67,175 @@ u8_error_t data_visible_set_load( data_visible_set_t *this_, data_row_id_t diagr
         data_visible_set_private_destroy_features( this_ );
         data_visible_set_private_destroy_relationships( this_ );
 
-        /* load diagram */
-        const u8_error_t db_err = data_database_reader_get_diagram_by_id ( db_reader, diagram_id, &((*this_).diagram) );
+        data_rules_t rules;
+        data_rules_init( &rules );
 
-        if ( u8_error_contains( db_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
+        /* load diagram */
         {
-            U8_LOG_ERROR( "U8_ERROR_STRING_BUFFER_EXCEEDED at loading a diagram" );
+            const u8_error_t diag_err
+                = data_database_reader_get_diagram_by_id ( db_reader, diagram_id, &((*this_).diagram) );
+            if ( u8_error_contains( diag_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
+            {
+                U8_LOG_ERROR( "U8_ERROR_STRING_BUFFER_EXCEEDED at loading a diagram" );
+            }
+            if ( u8_error_more_than( diag_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
+            {
+                /* error at loading */
+                data_diagram_reinit_empty( &((*this_).diagram) );
+            }
+            result |= diag_err;  /* collect error flags */
         }
-        if ( u8_error_more_than( db_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
-        {
-            /* error at loading */
-            data_diagram_reinit_empty( &((*this_).diagram) );
-        }
-        result |= db_err;  /* collect error flags */
 
         /* load classifiers */
-        (*this_).visible_classifier_count = 0;
-        data_visible_classifier_iterator_t visible_classifier_iterator;
-        result |= data_visible_classifier_iterator_init_empty( &visible_classifier_iterator );
-        result |= data_database_reader_get_visible_classifiers_by_diagram_id( db_reader,
-                                                                              diagram_id,
-                                                                              &visible_classifier_iterator
-                                                                            );
-        for ( int_fast32_t vc_idx = 0; (vc_idx < DATA_VISIBLE_SET_MAX_CLASSIFIERS) && data_visible_classifier_iterator_has_next( &visible_classifier_iterator ); vc_idx ++ )
         {
-            result |= data_visible_classifier_iterator_next( &visible_classifier_iterator, &((*this_).visible_classifiers[vc_idx]) );
-            (*this_).visible_classifier_count = vc_idx+1;
+            u8_error_t c_err = U8_ERROR_NONE;
+            (*this_).visible_classifier_count = 0;
+            data_visible_classifier_iterator_t visible_classifier_iterator;
+            c_err |= data_visible_classifier_iterator_init_empty( &visible_classifier_iterator );
+            c_err |= data_database_reader_get_visible_classifiers_by_diagram_id( db_reader,
+                                                                                 diagram_id,
+                                                                                 &visible_classifier_iterator
+                                                                               );
+            while ( data_visible_classifier_iterator_has_next( &visible_classifier_iterator )
+                && ( ! u8_error_more_than( c_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) ) )
+            {
+                if ( (*this_).visible_classifier_count < DATA_VISIBLE_SET_MAX_CLASSIFIERS )
+                {
+                    data_visible_classifier_t *const current_visible_classifier
+                        = &((*this_).visible_classifiers[(*this_).visible_classifier_count]);
+                    c_err |= data_visible_classifier_iterator_next( &visible_classifier_iterator,
+                                                                    current_visible_classifier
+                                                                  );
+                    if ( ! u8_error_more_than( c_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
+                    {
+                        (*this_).visible_classifier_count ++;
+                    }
+                }
+                else
+                {
+                    c_err |= U8_ERROR_ARRAY_BUFFER_EXCEEDED;
+                    U8_LOG_ERROR( "U8_ERROR_ARRAY_BUFFER_EXCEEDED at loading visible classifiers of a diagram" );
+                }
+            }
+            c_err |= data_visible_classifier_iterator_destroy( &visible_classifier_iterator );
+            result |= c_err;  /* collect error flags */
         }
-        if ( data_visible_classifier_iterator_has_next( &visible_classifier_iterator ) )
-        {
-            result |= U8_ERROR_ARRAY_BUFFER_EXCEEDED;
-            U8_LOG_ERROR( "U8_ERROR_ARRAY_BUFFER_EXCEEDED at loading visible classifiers of a diagram" );
-        }
-        result |= data_visible_classifier_iterator_destroy( &visible_classifier_iterator );
 
         /* load features */
-        (*this_).feature_count = 0;
-        data_feature_iterator_t feature_iterator;
-        result |= data_feature_iterator_init_empty( &feature_iterator );
-        result |= data_database_reader_get_features_by_diagram_id( db_reader,
-                                                                   diagram_id,
-                                                                   &feature_iterator
-                                                                 );
-        for ( int_fast32_t f_idx = 0; (f_idx < DATA_VISIBLE_SET_MAX_FEATURES) && data_feature_iterator_has_next( &feature_iterator ); f_idx ++ )
         {
-            result |= data_feature_iterator_next( &feature_iterator, &((*this_).features[f_idx]) );
-            (*this_).feature_count = f_idx+1;
+            u8_error_t f_err = U8_ERROR_NONE;
+            (*this_).feature_count = 0;
+            data_feature_iterator_t feature_iterator;
+            f_err |= data_feature_iterator_init_empty( &feature_iterator );
+            f_err |= data_database_reader_get_features_by_diagram_id( db_reader,
+                                                                      diagram_id,
+                                                                      &feature_iterator
+                                                                    );
+            while ( data_feature_iterator_has_next( &feature_iterator )
+                && ( ! u8_error_more_than( f_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) ) )
+            {
+                if ( (*this_).feature_count < DATA_VISIBLE_SET_MAX_FEATURES )
+                {
+                    data_feature_t *const current_feature = &((*this_).features[(*this_).feature_count]);
+                    f_err |= data_feature_iterator_next( &feature_iterator, current_feature );
+                    if ( ! u8_error_more_than( f_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
+                    {
+                        /* Ignore lifelines (== is_scenario) that do not belong to current diagram */
+                        const bool is_scenario =
+                            data_rules_feature_is_scenario_cond( &rules, data_feature_get_main_type( current_feature ) );
+                        bool is_foreign_scenario = is_scenario;
+                        for( uint_fast32_t vc_idx = 0; vc_idx < (*this_).visible_classifier_count; vc_idx ++ )
+                        {
+                            const data_diagramelement_t *const diag_ele
+                                = data_visible_classifier_get_diagramelement_const( &((*this_).visible_classifiers[vc_idx]) );
+                            const data_row_id_t focus_id = data_diagramelement_get_focused_feature_row_id( diag_ele );
+                            if ( data_feature_get_row_id( current_feature ) == focus_id )
+                            {
+                                is_foreign_scenario = false;
+                            }
+                        }
+                        if ( ! is_foreign_scenario ) /* either not a scenario or a scenario that is in scope of the diagram */
+                        {
+                            (*this_).feature_count++;
+                        }
+                        else
+                        {
+                            U8_TRACE_INFO_INT( "filtered a foreign scenario lifeline:",
+                                               data_feature_get_row_id( current_feature )
+                                             );
+                        }
+                    }
+                }
+                else
+                {
+                    f_err |= U8_ERROR_ARRAY_BUFFER_EXCEEDED;
+                    U8_LOG_ERROR( "U8_ERROR_ARRAY_BUFFER_EXCEEDED at loading features of a diagram" );
+                }
+            }
+            f_err |= data_feature_iterator_destroy( &feature_iterator );
+            result |= f_err;  /* collect error flags */
         }
-        if ( data_feature_iterator_has_next( &feature_iterator ) )
-        {
-            result |= U8_ERROR_ARRAY_BUFFER_EXCEEDED;
-            U8_LOG_ERROR( "U8_ERROR_ARRAY_BUFFER_EXCEEDED at loading features of a diagram" );
-        }
-        result |= data_feature_iterator_destroy( &feature_iterator );
 
         /* load relationships */
-        (*this_).relationship_count = 0;
-        data_relationship_iterator_t rel_iterator;
-        result |= data_relationship_iterator_init_empty( &rel_iterator );
-        result |= data_database_reader_get_relationships_by_diagram_id( db_reader,
-                                                                        diagram_id,
-                                                                        &rel_iterator
-                                                                      );
-        for ( int_fast32_t r_idx = 0; (r_idx < DATA_VISIBLE_SET_MAX_RELATIONSHIPS) && data_relationship_iterator_has_next( &rel_iterator ); r_idx ++ )
         {
-            result |= data_relationship_iterator_next( &rel_iterator, &((*this_).relationships[r_idx]) );
-            (*this_).relationship_count = r_idx+1;
+            u8_error_t r_err = U8_ERROR_NONE;
+            (*this_).relationship_count = 0;
+            data_relationship_iterator_t rel_iterator;
+            r_err |= data_relationship_iterator_init_empty( &rel_iterator );
+            r_err |= data_database_reader_get_relationships_by_diagram_id( db_reader,
+                                                                           diagram_id,
+                                                                           &rel_iterator
+                                                                         );
+            while ( data_relationship_iterator_has_next( &rel_iterator )
+                && ( ! u8_error_more_than( r_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) ) )
+            {
+                if ( (*this_).relationship_count < DATA_VISIBLE_SET_MAX_RELATIONSHIPS )
+                {
+                    data_relationship_t *const current_relationship = &((*this_).relationships[(*this_).relationship_count]);
+                    r_err |= data_relationship_iterator_next( &rel_iterator, current_relationship );
+                    if ( ! u8_error_more_than( r_err, U8_ERROR_STRING_BUFFER_EXCEEDED ) )
+                    {
+                        /* Ignore relationships that have not both ends in current diagram - e.g. messages between foreign lifelines */
+                        const data_row_id_t from_row_id = data_relationship_get_from_classifier_row_id( current_relationship );
+                        const data_row_id_t to_row_id = data_relationship_get_to_classifier_row_id( current_relationship );
+                        bool from_known = ( from_row_id == DATA_ROW_ID_VOID );
+                        bool to_known = ( to_row_id == DATA_ROW_ID_VOID );
+                        for( uint_fast32_t f_idx = 0; f_idx < (*this_).feature_count; f_idx ++ )
+                        {
+                            const data_feature_t *const probe_feature = &((*this_).features[f_idx]);
+                            const data_row_id_t probe_feat_id = data_feature_get_row_id( probe_feature );
+                            if ( probe_feat_id == from_row_id )
+                            {
+                                from_known = true;
+                            }
+                            if ( probe_feat_id == to_known )
+                            {
+                                to_known = true;
+                            }
+                        }
+                        if ( from_known && to_known )  /* check that both relationship ends are in this data_visible_set_t */
+                        {
+                            (*this_).relationship_count++;
+                        }
+                        else
+                        {
+                            U8_TRACE_INFO_INT( "filtered a foreign scenario message:",
+                                               data_relationship_get_row_id( current_relationship )
+                                             );
+                        }
+                    }
+                }
+                else
+                {
+                    r_err |= U8_ERROR_ARRAY_BUFFER_EXCEEDED;
+                    U8_LOG_ERROR( "U8_ERROR_ARRAY_BUFFER_EXCEEDED at loading relationships of a diagram" );
+                }
+            }
+            r_err |= data_relationship_iterator_destroy( &rel_iterator );
+            result |= r_err;  /* collect error flags */
         }
-        if ( data_feature_iterator_has_next( &feature_iterator ) )
-        {
-            result |= U8_ERROR_ARRAY_BUFFER_EXCEEDED;
-            U8_LOG_ERROR( "U8_ERROR_ARRAY_BUFFER_EXCEEDED at loading relationships of a diagram" );
-        }
-        result |= data_relationship_iterator_destroy( &rel_iterator );
+
+        data_rules_destroy( &rules );
     }
 
     /* update the containment cache */
