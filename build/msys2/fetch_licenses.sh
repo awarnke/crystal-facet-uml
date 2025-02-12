512fae9d0bb331193dda09662b384aeeeca406bf
Andreas Warnke <andreas.warnke@gmx.net>
2025-02-12 19:28:56 +0100

    documentation update

--- build/msys2/fetch_licenses.sh
+++ build/msys2/fetch_licenses.sh
@@ -9,12 +9,14 @@ do
 done
 
 file2=/ucrt64/bin/gdbus.exe
+echo $file2
 pkg2=$( pacman -Qo $file2 | sed -e 's/^\S* \S* \S* \S* \(\S*\).*$/\1/' 2>&1 )
 echo $pkg2
 pacman -Qi $pkg2
 echo --
 
 file3=/ucrt64/share/glib-2.0/schemas/org.gtk.gtk4.Settings.FileChooser.gschema.xml
+echo $file3
 pkg3=$( pacman -Qo $file3 | sed -e 's/^\S* \S* \S* \S* \(\S*\).*$/\1/' 2>&1 )
 echo $pkg3
 pacman -Qi $pkg3
