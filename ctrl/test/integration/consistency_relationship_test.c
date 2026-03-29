438492bf25e5d2be96b10cd2cf3c701de3bff297
Andreas Warnke <andreas.warnke@gmx.net>
2026-03-29 18:47:24 +0200

    2 test cases for consistency_relatinoship created.

--- ctrl/test/integration/consistency_relationship_test.c
+++ ctrl/test/integration/consistency_relationship_test.c
@@ -64,8 +64,89 @@ static void tear_down( test_fixture_t *fix )
 static test_case_result_t change_diagram_type( test_fixture_t *fix )
 {
     assert( fix != NULL );
+    ctrl_diagram_controller_t *diagram_ctrl;
+    diagram_ctrl = ctrl_controller_get_diagram_control_ptr( &((*fix).controller) );
+    tvec_setup_t test_environ;
+    tvec_setup_init( &test_environ, &((*fix).controller) );
 
-    return TEST_CASE_RESULT_ERR;
+    /* create 3 diagrams */
+    const data_row_t test_diagram
+    = tvec_setup_diagram( &test_environ, DATA_ROW_VOID, "test diag", DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM );
+    const data_row_t seq_diagram
+    = tvec_setup_diagram( &test_environ, test_diagram, "seq diag", DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM );
+    const data_row_t class_diagram
+    = tvec_setup_diagram( &test_environ, test_diagram, "class diag", DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM );
+
+    /* create 1 classifier and 3 diagramelements */
+    const data_row_t semi_classifier = tvec_setup_classifier( &test_environ, "semi classifier" );
+    tvec_setup_diagramelement( &test_environ, test_diagram, semi_classifier, DATA_ROW_VOID );
+    tvec_setup_diagramelement( &test_environ, seq_diagram, semi_classifier, DATA_ROW_VOID );
+
+    /* create 1 classifier and 3 diagramelements */
+    const data_row_t omni_classifier = tvec_setup_classifier( &test_environ, "omni classifier" );
+    tvec_setup_diagramelement( &test_environ, test_diagram, omni_classifier, DATA_ROW_VOID );
+    tvec_setup_diagramelement( &test_environ, seq_diagram, omni_classifier, DATA_ROW_VOID );
+    tvec_setup_diagramelement( &test_environ, class_diagram, omni_classifier, DATA_ROW_VOID );
+
+    /* create 1 feature */
+    const data_row_t omni_feature = tvec_setup_feature( &test_environ, omni_classifier, "omni feature" );
+
+    /* create 2 relationship */
+    const data_row_t semi_rel = tvec_setup_relationship( &test_environ,
+                                                         semi_classifier, DATA_ROW_VOID,
+                                                         omni_classifier, DATA_ROW_VOID,
+                                                         "semi to omni relation"
+                                                       );
+    const data_row_t omni_rel = tvec_setup_relationship( &test_environ,
+                                                         omni_classifier, omni_feature,
+                                                         omni_classifier, DATA_ROW_VOID,
+                                                         "omni to omni relation"
+                                                       );
+
+    tvec_setup_destroy( &test_environ );
+
+    /* test diag / DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM (< change to DATA_DIAGRAM_TYPE_BOX_DIAGRAM ) */
+    /*     semi classifier  ----------, */
+    /*     omni classifier  <----, <--' */
+    /*         omni feature  ----' */
+    /* seq diag / DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM */
+    /*     semi classifier  ----------, */
+    /*     omni classifier  <----, <--' */
+    /*         omni feature  ----' */
+    /* class diag / DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM */
+    /*     omni classifier  <----, */
+    /*         omni feature  ----' */
+
+    /* change the type of the test diagram */
+    data_stat_t statistics;
+    data_stat_init( &statistics );
+    const u8_error_t c_err
+    = ctrl_diagram_controller_update_diagram_type( diagram_ctrl,
+                                                   test_diagram,
+                                                   DATA_DIAGRAM_TYPE_BOX_DIAGRAM,
+                                                   &statistics
+    );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, c_err );
+
+    TEST_EXPECT_EQUAL_INT( 1, data_stat_get_series_count( &statistics, DATA_STAT_SERIES_MODIFIED ) );
+    TEST_EXPECT_EQUAL_INT( 1, data_stat_get_series_count( &statistics, DATA_STAT_SERIES_DELETED ) );
+    TEST_EXPECT_EQUAL_INT( 1, data_stat_get_table_count( &statistics, DATA_STAT_TABLE_DIAGRAM ) );
+    TEST_EXPECT_EQUAL_INT( 1, data_stat_get_table_count( &statistics, DATA_STAT_TABLE_RELATIONSHIP ) );
+    TEST_EXPECT_EQUAL_INT( 2, data_stat_get_total_count( &statistics ) );
+    data_stat_destroy( &statistics );
+
+    /* is semi_relationship deleted? */
+    data_relationship_t probe;
+    const u8_error_t rel_err1
+        = data_database_reader_get_relationship_by_id( &((*fix).db_reader), semi_rel, &probe );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_DB_STRUCTURE, rel_err1 );
+
+    /* is omni_relationship deleted? */
+    const u8_error_t rel_err2
+        = data_database_reader_get_relationship_by_id( &((*fix).db_reader), omni_rel, &probe );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, rel_err2 );
+
+    return TEST_CASE_RESULT_OK;
 }
 
 static test_case_result_t delete_diagramelement( test_fixture_t *fix )
@@ -76,66 +157,74 @@ static test_case_result_t delete_diagramelement( test_fixture_t *fix )
     tvec_setup_t test_environ;
     tvec_setup_init( &test_environ, &((*fix).controller) );
 
-    /* create 2 diagrams */
-    const data_row_t root_diagram
-        = tvec_setup_diagram( &test_environ, DATA_ROW_VOID, "root diag", DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM );
-    const data_row_t local_diagram
-        = tvec_setup_diagram( &test_environ, root_diagram, "local diag", DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM );
+    /* create 3 diagrams */
+    const data_row_t box_diagram
+    = tvec_setup_diagram( &test_environ, DATA_ROW_VOID, "box diag", DATA_DIAGRAM_TYPE_BOX_DIAGRAM );
+    const data_row_t seq_diagram
+    = tvec_setup_diagram( &test_environ, box_diagram, "seq diag", DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM );
+    const data_row_t class_diagram
+    = tvec_setup_diagram( &test_environ, box_diagram, "class diag", DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM );
 
-    /* create 3 classifiers */
+    /* create 1 classifier and 3 diagramelements */
     const data_row_t test_classifier = tvec_setup_classifier( &test_environ, "test classifier" );
-    const data_row_t omni_classifier = tvec_setup_classifier( &test_environ, "omni classifier" );
-    const data_row_t local_classifier = tvec_setup_classifier( &test_environ, "local classifier" );
-
-    /* create 5 diagramelements */
-    const data_row_t test_local_diagele
-        = tvec_setup_diagramelement( &test_environ, local_diagram, test_classifier, DATA_ROW_VOID );
-    tvec_setup_diagramelement( &test_environ, root_diagram, test_classifier, DATA_ROW_VOID );
-    tvec_setup_diagramelement( &test_environ, local_diagram, omni_classifier, DATA_ROW_VOID );
-    tvec_setup_diagramelement( &test_environ, root_diagram, omni_classifier, DATA_ROW_VOID );
-    tvec_setup_diagramelement( &test_environ, local_diagram, local_classifier, DATA_ROW_VOID );
+    tvec_setup_diagramelement( &test_environ, box_diagram, test_classifier, DATA_ROW_VOID );
+    const data_row_t test_seq_diagele
+        = tvec_setup_diagramelement( &test_environ, seq_diagram, test_classifier, DATA_ROW_VOID );
+    tvec_setup_diagramelement( &test_environ, class_diagram, test_classifier, DATA_ROW_VOID );
 
     /* create 1 feature */
     const data_row_t test_feature = tvec_setup_feature( &test_environ, test_classifier, "test feature" );
 
-    /* create 2 relationships */
-    const data_row_t double_rel = tvec_setup_relationship( &test_environ,
-                                                           test_classifier, test_feature,
-                                                           omni_classifier, DATA_ROW_VOID,
-                                                           "double relation"
-                                                         );
-    const data_row_t local_rel = tvec_setup_relationship( &test_environ,
-                                                          test_classifier, test_feature,
-                                                          local_classifier, DATA_ROW_VOID,
-                                                          "local relation"
-                                                        );
+    /* create 1 classifier and 3 diagramelements */
+    const data_row_t omni_classifier = tvec_setup_classifier( &test_environ, "omni classifier" );
+    tvec_setup_diagramelement( &test_environ, box_diagram, omni_classifier, DATA_ROW_VOID );
+    tvec_setup_diagramelement( &test_environ, seq_diagram, omni_classifier, DATA_ROW_VOID );
+    const data_row_t omni_class_diagele
+        = tvec_setup_diagramelement( &test_environ, class_diagram, omni_classifier, DATA_ROW_VOID );
+
+    /* create 1 relationship */
+    const data_row_t a_rel = tvec_setup_relationship( &test_environ,
+                                                      test_classifier, test_feature,
+                                                      omni_classifier, DATA_ROW_VOID,
+                                                      "a relation"
+                                                    );
+
     tvec_setup_destroy( &test_environ );
 
-    /* root diag / DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM */
+    /* box diag / DATA_DIAGRAM_TYPE_BOX_DIAGRAM */
     /*     test classifier */
     /*         test feature  ----, */
     /*     omni classifier  <----' */
-    /* local diag / DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM */
-    /*     test classifier   (< will be deleted) */
-    /*         test feature  ----,  ----, */
-    /*     omni classifier  <----'      | */
-    /*     local classifier  <----------' */
+    /* seq diag / DATA_DIAGRAM_TYPE_UML_SEQUENCE_DIAGRAM */
+    /*     test classifier   (< diagramelement will be deleted) */
+    /*         test feature  ----, */
+    /*     omni classifier  <----' */
+    /* class diag / DATA_DIAGRAM_TYPE_UML_CLASS_DIAGRAM */
+    /*     test classifier */
+    /*         test feature  ----, */
+    /*     omni classifier  <----' (< diagramelement will be deleted) */
 
-    /* delete the local diagramelement of the test classifier */
-    const u8_error_t c_err
-        = ctrl_diagram_controller_delete_diagramelement ( diagram_ctrl, test_local_diagele, CTRL_UNDO_REDO_ACTION_BOUNDARY_START_NEW );
-    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, c_err );
+    /* delete the test diagramelement @ seq diag */
+    const u8_error_t c_err1
+        = ctrl_diagram_controller_delete_diagramelement ( diagram_ctrl, test_seq_diagele, CTRL_UNDO_REDO_ACTION_BOUNDARY_START_NEW );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, c_err1 );
 
-    /* is local rel deleted? */
+    /* is the rel deleted? */
     data_relationship_t probe;
-    const u8_error_t local_err
-        = data_database_reader_get_relationship_by_id( &((*fix).db_reader), local_rel, &probe );
-    TEST_EXPECT_EQUAL_INT( U8_ERROR_DB_STRUCTURE, local_err );
+    const u8_error_t rel_err1
+        = data_database_reader_get_relationship_by_id( &((*fix).db_reader), a_rel, &probe );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, rel_err1 );
 
-    /* is double rel existing? */
-    const u8_error_t double_err
-        = data_database_reader_get_relationship_by_id( &((*fix).db_reader), double_rel, &probe );
-    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, double_err );  /* U8_ERROR_DB_STRUCTURE is expected now ... */
+    /* delete the omni diagramelement @ class diag */
+    const u8_error_t c_err2
+        = ctrl_diagram_controller_delete_diagramelement ( diagram_ctrl, omni_class_diagele, CTRL_UNDO_REDO_ACTION_BOUNDARY_START_NEW );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_NONE, c_err2 );
+
+    /* is the rel deleted? */
+    const u8_error_t rel_err2
+        = data_database_reader_get_relationship_by_id( &((*fix).db_reader), a_rel, &probe );
+    TEST_EXPECT_EQUAL_INT( U8_ERROR_DB_STRUCTURE, rel_err2 );
+
     return TEST_CASE_RESULT_OK;
 }
 
